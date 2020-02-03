27b10c4194a0b33933f1339ce0d5eda06da225ff
Francisco Javier Riquelme <franriquelme17@gmail.com>
2020-02-03 20:52:18 +0100

    Versión comentada

--- geometria/src/Rectangulo_FJRS.java
+++ geometria/src/Rectangulo_FJRS.java
@@ -1,12 +1,19 @@
 /**
-*
 * @author Francisco
 * Francisco Javier Riquelme Sánchez
 * Versión 1.2
 */
+
 public class Rectangulo extends FiguraGeometrica {
 	private double l1;
 	private double l2;
+	
+	/*
+	 * Constructor
+	 * @param tipoFigura
+	 * @param 1G
+	 * @param 1P
+	 */
 	
 	public Rectangulo(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
@@ -14,10 +21,18 @@ public class Rectangulo extends FiguraGeometrica {
 		l2 = lP;
 	}
 
+	/**
+	 * Mide area
+	 */
+	
 	@Override
 	public double area() {
 		return l1 * l2;
 	}
+	
+	/*
+	 * Mide perimetro
+	 */
 	
 	@Override
 	public double perimetro() {
